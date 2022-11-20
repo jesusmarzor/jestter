6b895ec611438cd680e7730962431714327c8ead
Jesus Martin <jesusmarzor28@gmail.com>
2022-11-20 17:09:16 +0100

    Add new interface

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,10 +1,15 @@
 /// <reference types="vite/client" />
 
-interface User {
+interface UserJestter {
     uid: string
     name: string | null
     email: string | null
     emailVerified: boolean
     phoneNumber: string | null
     photoURL: string | null
+}
+
+interface updateUserJestterData {
+    displayName?: string,
+    photoURL?: string
 }
