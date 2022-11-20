f23e476e19528df55b7552b5646631ae280d424b
Jesus Martin <jesusmarzor28@gmail.com>
2022-11-20 17:11:19 +0100

    Add new errors

--- src/utils/ERRORS_TYPE.ts
+++ src/utils/ERRORS_TYPE.ts
@@ -3,11 +3,13 @@ export const LOGIN_ERRORS_TYPE = {
     PASSWORD: "auth/wrong-password",
     MANY_REQUESTS: "auth/too-many-requests",
     INTERNAL: "auth/internal-error",
-    INVALID_EMAIL: "auth/invalid-email"
+    INVALID_EMAIL: "auth/invalid-email",
+    NOT_VERIFIED: "auth/not-verified"
 }
 
 export const REGISTER_ERRORS_TYPE = {
     EMAIL_IN_USE: "auth/email-already-in-use",
     MISSING_EMAIL: "auth/missing-email",
-    INVALID_EMAIL: "auth/invalid-email"
+    INVALID_EMAIL: "auth/invalid-email",
+    INVALID_NAME: "auth/invalid-name"
 }
